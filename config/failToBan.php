9b63a80f6599d61f4ea4b89e41e39996e812b3e1
Phyo-Aung-Naing-Tun <phyotun24@gmail.com>
2024-11-29 21:17:33 +0630

    added fail to ban config

---
+++ config/failToBan.php
@@ -0,0 +1,13 @@
+<?php
+ return [
+    'otp' => [
+        'min_attempt' => 5,
+        'duration_minutes' => 5,
+        'bann_months' => 6,
+    ],
+    'auth' => [
+        'min_attempt' => 5,
+        'duration_minutes' => 5,
+        'bann_months' => 6,
+    ]
+ ];
