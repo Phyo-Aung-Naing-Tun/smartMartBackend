b50bdcec043e15e93c80ed8fd6e171543315465c
Phyo-Aung-Naing-Tun <phyotun24@gmail.com>
2024-12-06 22:36:38 +0630

    added failtobanlog feature

--- app/Services/FailToBan/FailToBanLogService.php
+++ app/Services/FailToBan/FailToBanLogService.php
@@ -3,24 +3,27 @@
 namespace App\Services\FailToBan;
 
 use App\Contracts\FailToBan\FailToBanLogServiceInterface;
+use App\Models\FailToBan\FailToBan;
 use App\Models\FailToBan\FailToBanLog;
 use Illuminate\Http\Request;
 
 class FailToBanLogService implements FailToBanLogServiceInterface
 {
-    public function makeLog(Request $request, $type = null, $status = null)
+    public function makeLog(Request $request, $type = null, $status = null,FailToBan $failToBan = null)
     {
         $data = RequestParser::extract($request,$type);
         FailToBanLog::create([
             "ip" => $data['ip'],
+            "fail_to_ban_id" => $failToBan?->id,
             "action" => $data['action'],
             "payload" => $data['payload'],
             "path" => $data['path'],
-            "status" => '',
+            "status" => $status,
             "attempt_at" => now(),
             "platform" => $data['platform'],
             "method" => $data['method'],
             "origin" => $data['origin'],
+            "meta" => json_encode($request->all())
         ]);
     }
 }
