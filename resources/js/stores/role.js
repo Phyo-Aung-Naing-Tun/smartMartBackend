678fc070da7351b14f5b7a2e0f8fed15bbd40693
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2025-02-22 22:57:17 +0630

    fixed params passing in role.js

--- resources/js/stores/role.js
+++ resources/js/stores/role.js
@@ -1,30 +1,33 @@
 import { defineStore } from "pinia";
-import {ref} from 'vue';
+import { reactive, ref } from "vue";
 import apiClient from "@/axios/axiosConfig";
 
-export const useRolesStore = defineStore("roles",()=>{
-
+export const useRolesStore = defineStore("roles", () => {
     const roles = ref([]);
     const cacheRoles = ref([]);
     const key = ref("");
-    
-    function pullRoles(param){
-        let cacheRole = cacheRoles.value.find(role => role?.key == param);
 
-        if(cacheRole){            
+    function pullRoles(param) {
+        let cacheRole = cacheRoles.value.find((role) => role?.key == param);
+
+        if (cacheRole) {
             roles.value = cacheRole?.roles;
-        }else{            
-            apiClient.get('/roles', {search : param})
-            .then(response => {
-                console.log(response.data.data);
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        } else {
+            apiClient
+                .get("/roles", {
+                    params: {
+                        search: param,
+                    },
+                })
+                .then((response) => {
+                    console.log(response.data.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
         return param;
     }
 
-    return {roles,key,pullRoles};
-
+    return { roles, key, pullRoles };
 });
